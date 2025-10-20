15a587df8cba04021dc72989d347791a558d30ab
baciu <baciu@DORA>
2025-10-20 14:20:55 +0300

    Add project files.

---
+++ Data/Baciu_Dora_Lab2Context.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Baciu_Dora_Lab2.Models;
+
+namespace Baciu_Dora_Lab2.Data
+{
+    public class Baciu_Dora_Lab2Context : DbContext
+    {
+        public Baciu_Dora_Lab2Context (DbContextOptions<Baciu_Dora_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Baciu_Dora_Lab2.Models.Book> Book { get; set; } = default!;
+        public DbSet<Baciu_Dora_Lab2.Models.Publisher> Publisher { get; set; } = default!;
+    }
+}
---
+++ Pages/Books/Create.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Baciu_Dora_Lab2.Data;
+using Baciu_Dora_Lab2.Models;
+
+namespace Baciu_Dora_Lab2.Pages.Books
+{
+    public class CreateModel : PageModel
+    {
+        private readonly Baciu_Dora_Lab2.Data.Baciu_Dora_Lab2Context _context;
+
+        public CreateModel(Baciu_Dora_Lab2.Data.Baciu_Dora_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            ViewData["PublisherID"] = new SelectList(_context.Set<Publisher>(), "ID",
+"PublisherName");
+            return Page();
+        }
+
+        [BindProperty]
+        public Book Book { get; set; } = default!;
+
+        // For more information, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Book.Add(Book);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
